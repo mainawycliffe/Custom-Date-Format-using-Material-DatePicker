57c0ef158cf4df5cbea215820690ed6146abd8d3
Maina Wycliffe <wmmaina7@gmail.com>
2018-07-03 01:46:09 +0300

    Setup demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public firstDate = moment();
+  public secondDate = moment();
+  public thirdDate = moment();
+  public fourthDate = moment();
+  public fifthDate = moment();
+
+  ngOnInit() {}
 }
